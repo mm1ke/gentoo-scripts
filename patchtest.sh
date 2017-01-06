fe8ed57af551f526b393d11b7ad2ae6da5015b44
Michael Mair-Keimberger (asterix) <m.mairkeimberger@gmail.com>
2017-01-06 20:39:31 +0100

    patchtest: initial finding of patchnames with braces

--- patchtest.sh
+++ patchtest.sh
@@ -57,7 +57,7 @@ fi
 
 # python script to extract maintainers
 get_main(){
-	ret=`/usr/bin/python3 - $1 <<END
+	local ret=`/usr/bin/python3 - $1 <<END
 import xml.etree.ElementTree
 import sys
 pack = str(sys.argv[1])
@@ -77,6 +77,72 @@ END`
 	echo $ret
 }
 
+# python script to get permutations
+get_perm(){
+	local ret=`/usr/bin/python3 - "${1}" <<END
+import itertools
+import sys
+list=sys.argv[1].split(' ')
+for perm in itertools.permutations(list):
+	string= ','.join(perm)
+	print(string)
+END`
+	echo $ret
+}
+
+add_brace_patches(){
+
+	local work_list=("${patch_list[@]}")
+	local patch
+	local pat_found=()
+	local doubles=()
+	local count
+	
+	# expect patches in braces to use name+version or $PN
+	# thus the first 2 part (cut with -) ar similar
+	#
+	# first generate a list of files which fits the rule
+	for patch in "${work_list[@]}"; do
+		local deli=$(echo ${patch%.patch}|grep -o '-'|wc -w)
+		if [ $deli -ge 1 ]; then
+			pat_found+=("$(echo $patch|cut -d '-' -f1-$deli)")
+		fi
+	done
+	#echo "patternfound ${pat_found[@]}"
+	# create a list of duplicates
+	dup_list=()
+	for patch in "${pat_found[@]}"; do
+		# search for every element in the array
+		count=$(echo ${pat_found[@]}|grep -P -o "${patch}(?=\s|$)"|wc -w)
+		if [ $count -gt 1 ]; then
+			dup_list+=($patch)
+		fi
+	done
+	# remove duplicates from list
+	mapfile -t dup_list < <(printf '%s\n' "${dup_list[@]}"|sort -u)
+
+	#echo "dupfound ${dup_list[@]}"
+	# add matches to the patch_list
+	if [ -n "${dup_list}" ]; then
+		for patch in "${dup_list[@]}"; do
+			matches=()
+			for ffile in $(ls ${fullpath}/files/ |grep $patch); do
+				ffile="${ffile%.patch}"
+				#echo "filefound: $ffile"
+				matches+=("${ffile##*-}")
+			done
+			# set the maximum permutations number (5-6 works)
+			if [ ${#matches[@]} -le 5 ]; then
+				matches="${matches[@]}"
+				local perm_list=$(get_perm "$matches")
+				for search_patch in $perm_list; do
+					braces_patch_list+=("$(echo $patch-{$search_patch}.patch)")
+				done
+			fi
+		done
+	fi
+}
+
 
 ls -d ${cat}/${pac} |grep -E -v "distfiles|metadata|eclass" | while read -r line; do
 
@@ -86,62 +152,65 @@ ls -d ${cat}/${pac} |grep -E -v "distfiles|metadata|eclass" | while read -r line
 
 	# check if the patches folder exist
 	if [ -e ${fullpath}/files ]; then
-		for patchfile in ${fullpath}/files/*; do
-			# ignore sub-directories
-			if ! [ -d ${patchfile} ]; then
-				# patch basename
-				patchfile=${patchfile##*/}
-				# skip readme files
-				if [ "${patchfile}" == "README.gentoo" ]; then
-					continue
+		patch_list=()
+		for file in ${fullpath}/files/*; do
+			file="${file##*/}"
+			if ! [ -d ${file} ]; then
+				if  ! [ "${file}" = "README.gentoo" ]; then
+					patch_list+=("${file}")
 				fi
-				# check every ebuild
-				for ebuild in ${fullpath}/*.ebuild; do
-					# get ebuild detail
-					ebuild_full=$(basename ${ebuild%.*})
-					ebuild_version=$(echo ${ebuild_full/${package_name}}|cut -d'-' -f2)
-					ebuild_reversion=$(echo ${ebuild_full/${package_name}}|cut -d'-' -f3)
-					# create custom names to check
-					custom_name_1=${patchfile/${package_name}/${pn}}
-					custom_name_2=${patchfile/${package_name}-${ebuild_version}/${p}}
-					custom_name_4=${patchfile/${ebuild_version}/${pv}}
-					if [ -n "${ebuild_reversion}" ]; then
-						custom_name_3=${patchfile/${package_name}-${ebuild_version}-${ebuild_reversion}/${pf}}
-						custom_name_5=${patchfile/${ebuild_version}-${ebuild_reversion}/${pvr}}
-					else
-						custom_name_5=${patchfile/${ebuild_version}/${pvr}}
-					fi
-					# check ebuild for the custom names
-					if $(sed 's|"||g' ${ebuild} | grep ${patchfile} >/dev/null); then
-						found=true
-					elif $(sed 's|"||g' ${ebuild} | grep ${custom_name_1} >/dev/null); then
-						found=true
-					elif $(sed 's|"||g' ${ebuild} | grep ${custom_name_2} >/dev/null); then
-						found=true
-					elif [ -n "${ebuild_reversion}" ] && $(sed 's|"||g' ${ebuild} | grep ${custom_name_3} >/dev/null); then
-						found=true
-					elif $(sed 's|"||g' ${ebuild} | grep ${custom_name_5} >/dev/null); then
-						found=true
-					elif $(sed 's|"||g' ${ebuild} | grep ${custom_name_4} >/dev/null); then
-						found=true
-					else
-						found=false
-					fi
-
-					$found && break
-				done
-
-				if ! $found; then
-					if $print_main; then
-						main=$(get_main "${category}/${package_name}")
-						echo -e "$line: ${patchfile}\t\t${main}"
-					else
-						echo -e "$line: ${patchfile}"
-					fi
-				fi
-
-				found=false
 			fi
+		done
+		braces_patch_list=()
+		add_brace_patches
+		
+		for patchfile in "${patch_list[@]}"; do
+			# check every ebuild
+			for ebuild in ${fullpath}/*.ebuild; do
+				# get ebuild detail
+				ebuild_full=$(basename ${ebuild%.*})
+				ebuild_version=$(echo ${ebuild_full/${package_name}}|cut -d'-' -f2)
+				ebuild_reversion=$(echo ${ebuild_full/${package_name}}|cut -d'-' -f3)
+				# create custom names to check
+				custom_name_1=${patchfile/${package_name}/${pn}}
+				custom_name_2=${patchfile/${package_name}-${ebuild_version}/${p}}
+				custom_name_4=${patchfile/${ebuild_version}/${pv}}
+				if [ -n "${ebuild_reversion}" ]; then
+					custom_name_3=${patchfile/${package_name}-${ebuild_version}-${ebuild_reversion}/${pf}}
+					custom_name_5=${patchfile/${ebuild_version}-${ebuild_reversion}/${pvr}}
+				else
+					custom_name_5=${patchfile/${ebuild_version}/${pvr}}
+				fi
+				# check ebuild for the custom names
+				if $(sed 's|"||g' ${ebuild} | grep ${patchfile} >/dev/null); then
+					found=true
+				elif $(sed 's|"||g' ${ebuild} | grep ${custom_name_1} >/dev/null); then
+					found=true
+				elif $(sed 's|"||g' ${ebuild} | grep ${custom_name_2} >/dev/null); then
+					found=true
+				elif [ -n "${ebuild_reversion}" ] && $(sed 's|"||g' ${ebuild} | grep ${custom_name_3} >/dev/null); then
+					found=true
+				elif $(sed 's|"||g' ${ebuild} | grep ${custom_name_5} >/dev/null); then
+					found=true
+				elif $(sed 's|"||g' ${ebuild} | grep ${custom_name_4} >/dev/null); then
+					found=true
+				else
+					found=false
+				fi
+
+				$found && break
+			done
+
+			if ! $found; then
+				if $print_main; then
+					main=$(get_main "${category}/${package_name}")
+					echo -e "$line: ${patchfile}\t\t${main}"
+				else
+					echo -e "$line: ${patchfile}"
+				fi
+			fi
+
+			found=false
 		done
 	fi
 done
