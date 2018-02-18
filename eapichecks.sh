ee6bc3fb52c248c4e69e1191bbc11daefd282d31
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2018-02-18 12:49:40 +0100

    eapichecks: generate result for new dir

--- eapichecks.sh
+++ eapichecks.sh
@@ -157,5 +157,18 @@ if ${SCRIPT_MODE}; then
 	gen_sort_main ${WORKDIR}/bump_matchingkeywords/full.txt $(${ENABLE_GIT} && echo 8 || echo 6) ${WORKDIR}/bump_matchingkeywords/ ${DL}
 	gen_sort_pak ${WORKDIR}/bump_matchingkeywords/full.txt $(${ENABLE_GIT} && echo 3 || echo 2) ${WORKDIR}/bump_matchingkeywords/ ${DL}
 
+
+	mkdir -p ${WORKDIR/-/_}
+	gen_sort_main ${WORKDIR/-/_}/bump_needed/full.txt 5 ${WORKDIR/-/_}/bump_needed/ ${DL}
+	gen_sort_pak ${WORKDIR/-/_}/bump_needed/full.txt 3 ${WORKDIR/-/_}/bump_needed/ ${DL}
+
+	gen_sort_main ${WORKDIR/-/_}/bump_nonmatchingkeyword/full.txt $(${ENABLE_GIT} && echo 8 || echo 6) ${WORKDIR/-/_}/bump_nonmatchingkeyword/ ${DL}
+	gen_sort_pak ${WORKDIR/-/_}/bump_nonmatchingkeyword/full.txt $(${ENABLE_GIT} && echo 3 || echo 2) ${WORKDIR/-/_}/bump_nonmatchingkeyword/ ${DL}
+
+	gen_sort_main ${WORKDIR/-/_}/bump_matchingkeywords/full.txt $(${ENABLE_GIT} && echo 8 || echo 6) ${WORKDIR/-/_}/bump_matchingkeywords/ ${DL}
+	gen_sort_pak ${WORKDIR/-/_}/bump_matchingkeywords/full.txt $(${ENABLE_GIT} && echo 3 || echo 2) ${WORKDIR/-/_}/bump_matchingkeywords/ ${DL}
+	cp -r ${WORKDIR/-/_}/* /var/www/gentooqa.levelnine.at/results/checks/
+	rm -rf ${WORKDIR/-/_}
+
 	script_mode_copy
 fi
