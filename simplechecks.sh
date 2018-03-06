b049efe772863bf219129193cb4972340c48444d
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2018-03-06 18:51:31 +0100

    simplechecks: put vars in subfolders

--- simplechecks.sh
+++ simplechecks.sh
@@ -278,5 +278,10 @@ for var in ${_varibales}; do
 		-path ./metadata/\* -o \
 		-path ./eclass/\* -o \
 		-path ./.git/\* \) -prune -o -type f -name "*.ebuild" -exec egrep -l "^${var}=\" |^${var}=\".* \"$" {} \; | parallel main {}
+
+	mkdir -p /${WORKDIR}/${NAME}/sort-by-filter/${var}/
+	grep "${VARI}" /${WORKDIR}/${NAME}/full.txt > /${WORKDIR}/${NAME}/sort-by-filter/${var}/full.txt
+	gen_sort_main_v2 /${WORKDIR}/${NAME}/sort-by-filter/${var}/ 3
+	gen_sort_pak_v2 /${WORKDIR}/${NAME}/sort-by-filter/${var}/ 2
 done
 ${SCRIPT_MODE} && gen_sortings_v2 2 3
