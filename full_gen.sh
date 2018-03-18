017bc473160d9474d25918d35034aa18002552ef
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2018-03-18 11:27:39 +0100

    full_gen: cleanup

--- full_gen.sh
+++ full_gen.sh
@@ -24,12 +24,6 @@
 # create full bug lists per packages and maintainers
 
 startdir="$(dirname $(readlink -f $BASH_SOURCE))"
-#if [ -e ${startdir}/funcs.sh ]; then
-#	source ${startdir}/funcs.sh
-#else
-#	echo "Missing funcs.sh"
-#	exit 1
-#fi
 if [ -e ${startdir}/funcs-httpgen.sh ]; then
 	source ${startdir}/funcs-httpgen.sh
 else
@@ -86,44 +80,12 @@ for typ in IMP BUG FULL; do
 	rm -rf ${WORKDIR}
 done
 
-#mkdir -p ${WORKDIR}/full_list/{sort-by-package,sort-by-maintainer}
-#
-#for check in ${SITEDIR}/checks/*-*-*; do
-#	for main in $(ls ${check}/sort-by-maintainer/); do
-#		echo "<<< ${check##*/} >>>" >> ${WORKDIR}/full_list/sort-by-maintainer/${main}
-#		cat ${check}/sort-by-maintainer/${main} >> ${WORKDIR}/full_list/sort-by-maintainer/${main}
-#	done
-#done
-#
-#for check in ${SITEDIR}/checks/*-*-*; do
-#	for cat in $(ls ${check}/sort-by-package/); do
-#		mkdir -p ${WORKDIR}/full_list/sort-by-package/${cat}
-#		for pack in $(ls ${check}/sort-by-package/${cat}/); do
-#			echo "<<< ${check##*/} >>>" >> ${WORKDIR}/full_list/sort-by-package/${cat}/${pack}
-#			cat ${check}/sort-by-package/${cat}/${pack} >> ${WORKDIR}/full_list/sort-by-package/${cat}/${pack}
-#		done
-#	done
-#done
-#
-#for cat in $(ls ${WORKDIR}/full_list/sort-by-package/); do
-#	for pack in $(ls ${WORKDIR}/full_list/sort-by-package/${cat}/); do
-#		echo "<<< open bugs >>>" >> ${WORKDIR}/full_list/sort-by-package/${cat}/${pack}
-#		openbugs="$(get_bugs_full "${cat}/${pack::-4}")"
-#		echo "${openbugs}" >> ${WORKDIR}/full_list/sort-by-package/${cat}/${pack}
-#	done
-#done
-
-#[ -n "${SITEDIR}/full_lists/full_list" ] && rm -rf ${SITEDIR}/full_lists/full_list/
-#cp -r ${WORKDIR}/full_list ${SITEDIR}/full_lists/
-#rm -rf ${WORKDIR}
-
-
-# generate html output
+# generate html output (overview/results)
 gen_http_sort_main_v2 results ${SITEDIR}/checks > ${SITEDIR}/checks/index.html
 gen_http_sort_main_v2 results ${SITEDIR}/stats > ${SITEDIR}/stats/index.html
 gen_http_sort_main_v2 results ${SITEDIR}/full_lists > ${SITEDIR}/full_lists/index.html
 
-
+# generate html output (maintainer/results)
 for i in $(find ${SITEDIR}/checks/ -mindepth 1 -maxdepth 1 -type d); do
 	gen_http_sort_main_v2 main $i > ${i}/index.html
 done
