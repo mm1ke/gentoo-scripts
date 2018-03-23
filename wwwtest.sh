11b864ca9dc515dbf8919636b72864749791418e
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2018-03-23 20:33:07 +0100

    wwwtest: put sourcing on top to allow overriding sourced variables

--- wwwtest.sh
+++ wwwtest.sh
@@ -23,6 +23,14 @@
 # Discription:
 # simple scirpt to find broken websites
 
+startdir="$(dirname $(readlink -f $BASH_SOURCE))"
+if [ -e ${startdir}/funcs.sh ]; then
+	source ${startdir}/funcs.sh
+else
+	echo "Missing funcs.sh"
+	exit 1
+fi
+
 SCRIPT_MODE=false
 SCRIPT_NAME="wwwtest"
 SCRIPT_SHORT="WWT"
@@ -48,13 +56,6 @@ array_names(){
 }
 array_names
 
-startdir="$(dirname $(readlink -f $BASH_SOURCE))"
-if [ -e ${startdir}/funcs.sh ]; then
-	source ${startdir}/funcs.sh
-else
-	echo "Missing funcs.sh"
-	exit 1
-fi
 
 if [ "$(hostname)" = vs4 ]; then
 	SCRIPT_MODE=true
