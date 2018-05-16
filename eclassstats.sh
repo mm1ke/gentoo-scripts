a765387a4f6f680f6df30181e8b1ee3df0b7c5ca
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2018-05-16 18:49:59 +0200

    new script: eclassstats.sh

---
+++ eclassstats.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# Filename: eclassstats.sh
+# Autor: Michael Mair-Keimberger (m DOT mairkeimberger AT gmail DOT com)
+# Date: 05/05/2018
+
+# Copyright (C) 2018  Michael Mair-Keimberger
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# Discription:
+# lists eclass uses of the tree
+
+#override PORTTREE,SCRIPT_MODE,SITEDIR settings
+#export PORTTREE=/usr/portage/
+#export SCRIPT_MODE=true
+#export SITEDIR="${HOME}/eclassstats/"
+
+# load repo specific settings
+startdir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+if [ -e ${startdir}/repo ]; then
+	source ${startdir}/repo
+fi
+
+# get dirpath and load funcs.sh
+realdir="$(dirname $(readlink -f $BASH_SOURCE))"
+if [ -e ${realdir}/_funcs.sh ]; then
+	source ${realdir}/_funcs.sh
+else
+	echo "Missing _funcs.sh"
+	exit 1
+fi
+
+#
+### IMPORTANT SETTINGS START ###
+#
+SCRIPT_NAME="eclassstats"
+SCRIPT_SHORT="ECS"
+WORKDIR="/tmp/${SCRIPT_NAME}-${RANDOM}"
+
+array_names(){
+	RUNNING_CHECKS=(
+		"${WORKDIR}/${SCRIPT_SHORT}-STA-ebuild_eclass_statistics"				#Index 0
+	)
+}
+array_names
+#
+### IMPORTANT SETTINGS STOP ###
+#
+
+main() {
+	array_names
+	local relative_path=${1}
+	local category="$(echo ${relative_path}|cut -d'/' -f2)"
+	local package="$(echo ${relative_path}|cut -d'/' -f3)"
+	local filename="$(echo ${relative_path}|cut -d'/' -f4)"
+	local packagename="${filename%.*}"
+	local full_path="${PORTTREE}/${category}/${package}"
+	local full_path_ebuild="${PORTTREE}/${category}/${package}/${filename}"
+	local maintainer="$(get_main_min "${category}/${package}")"
+	local full_md5path="${PORTTREE}/metadata/md5-cache/${category}/${packagename}"
+
+	ebuild_eclass_file=$(get_eclasses_file ${full_md5path} ${full_path_ebuild})
+	#ebuild_eclass_real=$(get_eclasses_real ${full_md5path})
+
+	if [ -n "${ebuild_eclass_file}" ]; then
+		if ${SCRIPT_MODE}; then
+			echo "${category}/${package}${DL}${filename}${DL}${ebuild_eclass_file}${DL}${maintainer}" >> ${RUNNING_CHECKS[0]}/full.txt
+		else
+			echo "${category}/${package}${DL}${filename}${DL}${ebuild_eclass_file}${DL}${maintainer}"
+		fi
+	fi
+}
+
+# set the search depth
+depth_set ${1}
+# switch to the PORTTREE dir
+cd ${PORTTREE}
+# export important variables
+export WORKDIR SCRIPT_SHORT
+export -f main array_names
+
+${SCRIPT_MODE} && mkdir -p ${RUNNING_CHECKS[@]}
+
+
+if ${SCRIPT_MODE}; then
+	# create a list and create corresponding folders and files of all available
+	# eclasses before running the check.
+	# this way we also see eclasses without customers
+	if ${TREE_IS_MASTER}; then
+		eclass_list=( $(ls ${PORTTREE}/eclass/*.eclass) )
+		eclass_list=( ${eclass_list[@]##*/} )
+		for ecl in ${eclass_list[@]}; do
+			mkdir -p ${RUNNING_CHECKS[0]}/sort-by-filter/${ecl}
+			touch ${RUNNING_CHECKS[0]}/sort-by-filter/${ecl}/full.txt
+		done
+	fi
+fi
+
+find ./${level} \( \
+	-path ./scripts/\* -o \
+	-path ./profiles/\* -o \
+	-path ./packages/\* -o \
+	-path ./licenses/\* -o \
+	-path ./distfiles/\* -o \
+	-path ./metadata/\* -o \
+	-path ./eclass/\* -o \
+	-path ./.git/\* \) -prune -o -type f -name "*.ebuild" -exec egrep -l 'inherit' {} \; | parallel main {}
+
+if ${SCRIPT_MODE}; then
+
+	for file in $(cat ${RUNNING_CHECKS[0]}/full.txt); do
+		for ec in $(echo ${file}|cut -d'|' -f3|tr ':' ' '); do
+			mkdir -p ${RUNNING_CHECKS[0]}/sort-by-filter/${ec}.eclass
+			echo ${file} >> ${RUNNING_CHECKS[0]}/sort-by-filter/${ec}.eclass/full.txt
+		done
+	done
+
+	gen_sort_main_v2 ${RUNNING_CHECKS[0]} 4
+	gen_sort_pak_v2 ${RUNNING_CHECKS[0]} 1
+
+	copy_checks stats
+	rm -rf ${WORKDIR}
+fi
