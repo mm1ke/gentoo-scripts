581ab2981f3011c26a3a5d99a3667e408b952288
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2018-05-24 22:09:36 +0200

    new script: deadeclasses

---
+++ deadeclasses.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+# Filename: deadeclasses.sh
+# Autor: Michael Mair-Keimberger (m DOT mairkeimberger AT gmail DOT com)
+# Date: 24/05/2018
+
+# Copyright (C) 2018  Michael Mair-Keimberger
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# Discription:
+# lists deprecated eclasses
+
+#override PORTTREE,SCRIPT_MODE,SITEDIR settings
+#export PORTTREE=/usr/portage/
+#export SCRIPT_MODE=true
+#export SITEDIR="${HOME}/deadeclasses/"
+
+# load repo specific settings
+startdir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+if [ -e ${startdir}/repo ]; then
+	source ${startdir}/repo
+fi
+
+# get dirpath and load funcs.sh
+realdir="$(dirname $(readlink -f $BASH_SOURCE))"
+if [ -e ${realdir}/_funcs.sh ]; then
+	source ${realdir}/_funcs.sh
+else
+	echo "Missing _funcs.sh"
+	exit 1
+fi
+
+#
+### IMPORTANT SETTINGS START ###
+#
+SCRIPT_NAME="deadeclasses"
+SCRIPT_SHORT="DEL"
+WORKDIR="/tmp/${SCRIPT_NAME}-${RANDOM}"
+
+array_names(){
+	RUNNING_CHECKS=(
+		"${WORKDIR}/${SCRIPT_SHORT}-BUG-ebuild_deprecated_eclasses"				#Index 0
+	)
+}
+array_names
+#
+### IMPORTANT SETTINGS STOP ###
+#
+
+main() {
+	array_names
+	local relative_path=${1}
+	local category="$(echo ${relative_path}|cut -d'/' -f2)"
+	local package="$(echo ${relative_path}|cut -d'/' -f3)"
+	local filename="$(echo ${relative_path}|cut -d'/' -f4)"
+	local packagename="${filename%.*}"
+	local full_path="${PORTTREE}/${category}/${package}"
+	local full_path_ebuild="${PORTTREE}/${category}/${package}/${filename}"
+	local maintainer="$(get_main_min "${category}/${package}")"
+	local full_md5path="${PORTTREE}/metadata/md5-cache/${category}/${packagename}"
+
+	local dead_eclasses="git-2 games fdo-mime ltprune base versionator"
+	local found_usage=( )
+
+	for eclass in ${dead_eclasses}; do
+		if $(check_eclasses_usage ${full_path_ebuild} ${eclass}); then
+			found_usage+=( ${eclass} )
+		fi
+	done
+
+
+	if [ -n "${found_usage}" ]; then
+		if ${SCRIPT_MODE}; then
+			echo "$(get_eapi ${full_path_ebuild})${DL}${category}/${package}${DL}${filename}${DL}$(echo ${found_usage}|tr ' ' ':')${DL}${maintainer}" >> ${RUNNING_CHECKS[0]}/full.txt
+		else
+			echo "$(get_eapi ${full_path_ebuild})${DL}${category}/${package}${DL}${filename}${DL}$(echo ${found_usage}|tr ' ' ':')${DL}${maintainer}"
+		fi
+	fi
+}
+
+# set the search depth
+depth_set ${1}
+# switch to the PORTTREE dir
+cd ${PORTTREE}
+# export important variables
+export WORKDIR SCRIPT_SHORT
+export -f main array_names
+
+${SCRIPT_MODE} && mkdir -p ${RUNNING_CHECKS[@]}
+
+
+find ./${level} \( \
+	-path ./scripts/\* -o \
+	-path ./profiles/\* -o \
+	-path ./packages/\* -o \
+	-path ./licenses/\* -o \
+	-path ./distfiles/\* -o \
+	-path ./metadata/\* -o \
+	-path ./eclass/\* -o \
+	-path ./.git/\* \) -prune -o -type f -name "*.ebuild" -exec egrep -l 'inherit' {} \; | parallel main {}
+
+if ${SCRIPT_MODE}; then
+
+	for file in $(cat ${RUNNING_CHECKS[0]}/full.txt); do
+		for ec in $(echo ${file}|cut -d'|' -f4|tr ':' ' '); do
+			mkdir -p ${RUNNING_CHECKS[0]}/sort-by-filter/${ec}.eclass
+			echo ${file} >> ${RUNNING_CHECKS[0]}/sort-by-filter/${ec}.eclass/full.txt
+		done
+	done
+
+	for ecd in $(ls ${RUNNING_CHECKS[0]}/sort-by-filter/); do
+		gen_sort_main_v2 ${RUNNING_CHECKS[0]}/sort-by-filter/${ecd} 5
+		gen_sort_pak_v2 ${RUNNING_CHECKS[0]}/sort-by-filter/${ecd} 2
+	done
+
+	gen_sort_main_v2 ${RUNNING_CHECKS[0]} 5
+	gen_sort_pak_v2 ${RUNNING_CHECKS[0]} 2
+
+	copy_checks stats
+	rm -rf ${WORKDIR}
+fi
