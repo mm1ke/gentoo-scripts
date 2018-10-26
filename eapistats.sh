671bb0b08d2de34185fa0f49c388f79382081698
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2018-10-26 19:27:55 +0200

    eapistats: fix find_func

--- eapistats.sh
+++ eapistats.sh
@@ -120,7 +120,7 @@ find_func(){
 	fi
 
 	find ${searchp[@]} -mindepth $(expr ${MIND} + 1) -maxdepth $(expr ${MAXD} + 1) \
-		-type f -name "*.ebuild" -exec egrep -l "DEPEND" {} \; | parallel main {}
+		-type f -name "*.ebuild" -print | parallel main {}
 }
 
 gen_results() {
