3b6828792485df7762dda4288733e4383bdd5580
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2018-10-29 15:58:50 +0100

    patchtest: implement diff mode, new search func, sort result

--- patchtest.sh
+++ patchtest.sh
@@ -286,8 +286,8 @@ main(){
 	local prechecks=true
 
 	local full_package=${1}
-	local category="$(echo ${full_package}|cut -d'/' -f2)"
-	package="$(echo ${full_package}|cut -d'/' -f3)"
+	local category="$(echo ${full_package}|cut -d'/' -f1)"
+	package="$(echo ${full_package}|cut -d'/' -f2)"
 
 	fullpath="/${PORTTREE}/${full_package}"
 
@@ -505,35 +505,99 @@ export -f main get_perm get_main_min array_names
 export WORKDIR startdir SCRIPT_SHORT
 ${SCRIPT_MODE} && mkdir -p ${RUNNING_CHECKS[@]}
 
+find_func(){
+	if [ "${1}" = "full" ]; then
+		searchp=( $(find ${PORTTREE} -mindepth 1 -maxdepth 1 \
+			-type d -regextype sed -regex "./*[a-z0-9].*-[a-z0-9].*" -printf '%P\n') )
+		# virtual wouldn't be included by the find command, adding it manually if
+		# it's present
+		[ -e ${PORTTREE}/virtual ] && searchp+=( "virtual" )
+		# full provides only categories so we need maxd=2 and mind=2
+		# setting both vars to 1 because the find command adds 1 anyway
+		MAXD=1
+		MIND=1
+	elif [ "${1}" = "diff" ]; then
+		searchp=( $(sed -e 's/^.//' ${TODAYCHECKS}) )
+		# diff provides categories/package so we need maxd=1 and mind=1
+		# setting both vars to 0 because the find command adds 1 anyway
+		MAXD=0
+		MIND=0
+	elif [ -z "${1}" ]; then
+		echo "No directory given. Please fix your script"
+		exit 1
+	else
+		searchp=( ${1} )
+	fi
+
 # Dont use parallel if DEBUG is enabled
-if ${DEBUG}; then
-	find ./${level} -mindepth $MIND -maxdepth $MAXD \( \
-		-path ./scripts/\* -o \
-		-path ./profiles/\* -o \
-		-path ./packages/\* -o \
-		-path ./licenses/\* -o \
-		-path ./distfiles/\* -o \
-		-path ./metadata/\* -o \
-		-path ./eclass/\* -o \
-		-path ./.git/\* \) -prune -o -type d -print | while read -r line; do
-		main ${line}
-	done
+	if ${DEBUG}; then
+		find ${searchp[@]} -mindepth ${MIND} -maxdepth ${MAXD} \
+			-type d -print | while read -r line; do
+			main ${line}
+		done
+	else
+		find ${searchp[@]} -mindepth ${MIND} -maxdepth ${MAXD} \
+			-type d -print | parallel main {}
+	fi
+}
+
+gen_results() {
+	if ${SCRIPT_MODE}; then
+		sort_result ${RUNNING_CHECKS[0]}
+		gen_sort_main_v2 ${RUNNING_CHECKS[0]} 3
+		gen_sort_pak_v2 ${RUNNING_CHECKS[0]} 1
+
+		copy_checks ${SCRIPT_TYPE}
+	fi
+}
+
+if [ "${1}" = "diff" ]; then
+	# if /tmp/${SCRIPT_NAME} exist run in normal mode
+	# this way it's possible to override the diff mode
+	# this is usefull when the script got updates which should run
+	# on the whole tree
+	if ! [ -e "/tmp/${SCRIPT_NAME}" ]; then
+
+		TODAYCHECKS="${HASHTREE}/results/results-$(date -I).log"
+		# only run diff mode if todaychecks exist and doesn't have zero bytes
+		if [ -s ${TODAYCHECKS} ]; then
+
+			# we need to copy all existing results first and remove packages which
+			# were changed (listed in TODAYCHECKS). If no results file exists, do
+			# nothing - the script would create a new one anyway
+			for oldfull in ${RUNNING_CHECKS[@]}; do
+				# SCRIPT_TYPE isn't used in the ebuilds usually,
+				# thus it has to be set with the other important variables
+				#
+				# first set the full.txt path from the old log
+				OLDLOG="${SITEDIR}/${SCRIPT_TYPE}/${oldfull/${WORKDIR}/}/full.txt"
+				# check if the oldlog exist (don't have to be)
+				if [ -e ${OLDLOG} ]; then
+					# copy old result file to workdir and filter the result
+					cp ${OLDLOG} ${oldfull}/
+					for cpak in $(cat ${TODAYCHECKS}); do
+						# the substring replacement is important (replaces '/' to '\/'), otherwise the sed command
+						# will fail because '/' aren't escapted. also remove first slash
+						pakcat="${cpak:1}"
+						sed -i "/${pakcat//\//\\/}${DL}/d" ${oldfull}/full.txt
+					done
+				fi
+			done
+
+			# run the script only on the changed packages
+			find_func ${1}
+			# remove dropped packages
+			diff_rm_dropped_paks 1
+			gen_results
+		fi
+	else
+		find_func full
+		gen_results
+	fi
 else
-	find ./${level} -mindepth $MIND -maxdepth $MAXD \( \
-		-path ./scripts/\* -o \
-		-path ./profiles/\* -o \
-		-path ./packages/\* -o \
-		-path ./licenses/\* -o \
-		-path ./distfiles/\* -o \
-		-path ./metadata/\* -o \
-		-path ./eclass/\* -o \
-		-path ./.git/\* \) -prune -o -type d -print | parallel main {}
+	find_func ${1}
+	gen_results
 fi
 
-if ${SCRIPT_MODE}; then
-	gen_sort_main_v2 ${RUNNING_CHECKS[0]} 3
-	gen_sort_pak_v2 ${RUNNING_CHECKS[0]} 1
 
-	copy_checks ${SCRIPT_TYPE}
-	rm -rf ${WORKDIR}
-fi
+${SCRIPT_MODE} && rm -rf ${WORKDIR}
