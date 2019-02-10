95b87652c62a5adaaa39cad537a2e0ee5190ba47
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2019-02-10 13:37:00 +0100

    trailwhite: use depth_set_v2 + cleanup

--- trailwhite.sh
+++ trailwhite.sh
@@ -95,45 +95,17 @@ main() {
 	fi
 }
 
-depth_set ${1}
-cd ${PORTTREE}
-${SCRIPT_MODE} && mkdir -p ${RUNNING_CHECKS[@]}
-export -f main get_main_min array_names
-export WORKDIR SCRIPT_SHORT
 
 find_func(){
-	if [ "${1}" = "full" ]; then
-		searchp=( $(find ${PORTTREE} -mindepth 1 -maxdepth 1 \
-			-type d -regextype sed -regex "./*[a-z0-9].*-[a-z0-9].*" -printf '%P\n') )
-		# virtual wouldn't be included by the find command, adding it manually if
-		# it's present
-		[ -e ${PORTTREE}/virtual ] && searchp+=( "virtual" )
-		# full provides only categories so we need maxd=2 and mind=2
-		# setting both vars to 1 because the find command adds 1 anyway
-		MAXD=1
-		MIND=1
-	elif [ "${1}" = "diff" ]; then
-		searchp=( $(sed -e 's/^.//' ${TODAYCHECKS}) )
-		# diff provides categories/package so we need maxd=1 and mind=1
-		# setting both vars to 0 because the find command adds 1 anyway
-		MAXD=0
-		MIND=0
-	elif [ -z "${1}" ]; then
-		echo "No directory given. Please fix your script"
-		exit 1
-	else
-		searchp=( ${1} )
-	fi
-
 	find ${searchp[@]} -mindepth $(expr ${MIND} + 1) -maxdepth $(expr ${MAXD} + 1) \
 		-type f -name "*.ebuild" -print | parallel main {}
 }
 
 gen_results(){
 	if ${SCRIPT_MODE}; then
-		sort_result ${RUNNING_CHECKS[0]}
-		gen_sort_main_v2 ${RUNNING_CHECKS[0]} 4
-		gen_sort_pak_v2 ${RUNNING_CHECKS[0]} 1
+		sort_result_v2
+		gen_sort_main_v3
+		gen_sort_pak_v3
 
 		for file in $(cat ${RUNNING_CHECKS[0]}/full.txt); do
 			for ec in $(echo ${file}|cut -d'|' -f3|tr ':' ' '); do
@@ -143,60 +115,17 @@ gen_results(){
 		done
 
 		for ecd in $(ls ${RUNNING_CHECKS[0]}/sort-by-filter/); do
-			gen_sort_main_v2 ${RUNNING_CHECKS[0]}/sort-by-filter/${ecd} 4
-			gen_sort_pak_v2 ${RUNNING_CHECKS[0]}/sort-by-filter/${ecd} 1
+			gen_sort_main_v3 ${RUNNING_CHECKS[0]}/sort-by-filter/${ecd}
+			gen_sort_pak_v3 ${RUNNING_CHECKS[0]}/sort-by-filter/${ecd}
 		done
 
 		copy_checks ${SCRIPT_TYPE}
 	fi
 }
 
-if [ "${1}" = "diff" ]; then
-	# if /tmp/${SCRIPT_NAME} exist run in normal mode
-	# this way it's possible to override the diff mode
-	# this is usefull when the script got updates which should run
-	# on the whole tree
-	if ! [ -e "/tmp/${SCRIPT_NAME}" ]; then
-
-		TODAYCHECKS="${HASHTREE}/results/results-$(date -I).log"
-		# only run diff mode if todaychecks exist and doesn't have zero bytes
-		if [ -s ${TODAYCHECKS} ]; then
-
-			# we need to copy all existing results first and remove packages which
-			# were changed (listed in TODAYCHECKS). If no results file exists, do
-			# nothing - the script would create a new one anyway
-			for oldfull in ${RUNNING_CHECKS[@]}; do
-				# SCRIPT_TYPE isn't used in the ebuilds usually,
-				# thus it has to be set with the other important variables
-				#
-				# first set the full.txt path from the old log
-				OLDLOG="${SITEDIR}/${SCRIPT_TYPE}/${oldfull/${WORKDIR}/}/full.txt"
-				# check if the oldlog exist (don't have to be)
-				if [ -e ${OLDLOG} ]; then
-					# copy old result file to workdir and filter the result
-					cp ${OLDLOG} ${oldfull}/
-					for cpak in $(cat ${TODAYCHECKS}); do
-						# the substring replacement is important (replaces '/' to '\/'), otherwise the sed command
-						# will fail because '/' aren't escapted. also remove first slash
-						pakcat="${cpak:1}"
-						sed -i "/${pakcat//\//\\/}${DL}/d" ${oldfull}/full.txt
-					done
-				fi
-			done
-
-			# run the script only on the changed packages
-			find_func ${1}
-			# remove dropped packages
-			diff_rm_dropped_paks 1
-			gen_results
-		fi
-	else
-		find_func full
-		gen_results
-	fi
-else
-	find_func ${1}
-	gen_results
-fi
-
+cd ${PORTTREE}
+export -f main get_main_min array_names
+export WORKDIR SCRIPT_SHORT
+${SCRIPT_MODE} && mkdir -p ${RUNNING_CHECKS[@]}
+depth_set_v2 ${1}
 ${SCRIPT_MODE} && rm -rf ${WORKDIR}
