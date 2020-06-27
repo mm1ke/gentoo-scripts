967f1f8481d20c858a2550d419b00ae9d04d7a65
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2020-06-27 11:05:07 +0200

    treehashgen: don't check for directory as we create it anyway

--- treehashgen.sh
+++ treehashgen.sh
@@ -57,58 +57,56 @@ WORKDIR="/var/tmp/${SCRIPT_NAME}/${REPO}"
 date_today="$(date -I)"
 
 hash_start(){
-	# check if hashtree directory exists
-	if [ -d ${HASHTREE} ]; then
-		# only run if there doesn't exists a result for today
-		if ! [ -e ${HASHTREE}/full-${date_today}.log ]; then
-			mkdir -p ${WORKDIR}
+	mkdir -p ${HASHTREE}
+	# only run if there doesn't exists a result for today
+	if ! [ -e ${HASHTREE}/full-${date_today}.log ]; then
+		mkdir -p ${WORKDIR}
 
-			# generate hashes for every package
-			# list every category
-			local searchp="${PORTTREE}/*-*"
-			local searchp=( $(find ${PORTTREE} -mindepth 1 -maxdepth 1 -type d -regextype sed -regex "./*[a-z0-9].*-[a-z0-9].*") )
-			[ -d "${PORTTREE}/virtual" ] && searchp+=( "${PORTTREE%/}/virtual" )
+		# generate hashes for every package
+		# list every category
+		local searchp="${PORTTREE}/*-*"
+		local searchp=( $(find ${PORTTREE} -mindepth 1 -maxdepth 1 -type d -regextype sed -regex "./*[a-z0-9].*-[a-z0-9].*") )
+		[ -d "${PORTTREE}/virtual" ] && searchp+=( "${PORTTREE%/}/virtual" )
 
-			for cate in ${searchp[@]}; do
-				for paka in $(find ${cate} -mindepth 1 -maxdepth 1 -type d); do
-					mkdir -p ${WORKDIR}/${paka/${PORTTREE}/}
-					# list all files in each directory and create hash
-					find ${paka} -type f -exec xxh64sum {} \; > ${WORKDIR}/${paka/${PORTTREE}/}/package-xhash.xha
-					#echo ${paka} >> /tmp/package-ng-new.log
-				done
+		for cate in ${searchp[@]}; do
+			for paka in $(find ${cate} -mindepth 1 -maxdepth 1 -type d); do
+				mkdir -p ${WORKDIR}/${paka/${PORTTREE}/}
+				# list all files in each directory and create hash
+				find ${paka} -type f -exec xxh64sum {} \; > ${WORKDIR}/${paka/${PORTTREE}/}/package-xhash.xha
+				#echo ${paka} >> /tmp/package-ng-new.log
 			done
+		done
 
-			# generate hashes for every category based on the package hashes
+		# generate hashes for every category based on the package hashes
+		for cat in $(find ${WORKDIR} -mindepth 1 -maxdepth 1 -type d); do
+			find ${cat} -mindepth 2 -type f -name *.xha -exec xxh64sum {} \; \
+				| tee -a ${cat}/category-xhash.xha ${HASHTREE}/full-${date_today}.log >/dev/null
+		done
+		# generate hash for the full tree
+		find ${WORKDIR} -mindepth 2 -maxdepth 2 -type f -name *.xha -exec xxh64sum {} \; \
+			| tee -a ${WORKDIR}/tree-xhash.xha ${HASHTREE}/full-${date_today}.log >/dev/null
+		# generate results file, based on the diffs from the full-last.log
+		# if this file doesn't exist nothing happen.
+		if [ -e ${HASHTREE}/full-last.log ]; then
+			mkdir -p ${HASHTREE}/results
+			touch ${HASHTREE}/results/results-${date_today}.log
+			# list every category, save hashes of today and yesterday, compare and
+			# if it doesn't match, check every package in that category
 			for cat in $(find ${WORKDIR} -mindepth 1 -maxdepth 1 -type d); do
-				find ${cat} -mindepth 2 -type f -name *.xha -exec xxh64sum {} \; \
-					| tee -a ${cat}/category-xhash.xha ${HASHTREE}/full-${date_today}.log >/dev/null
+				cat_hash_last="$(grep ${cat}/category-xhash.xha ${HASHTREE}/full-last.log | cut -d ' ' -f1)"
+				cat_hash_today="$(grep ${cat}/category-xhash.xha ${HASHTREE}/full-${date_today}.log| cut -d' ' -f1)"
+				if ! [ "${cat_hash_last}" = "${cat_hash_today}" ]; then
+					for pak in $(find ${cat} -mindepth 1 -maxdepth 1 -type d); do
+						pak_hash_last="$(grep ${pak}/package-xhash.xha ${HASHTREE}/full-last.log | cut -d ' ' -f1)"
+						pak_hash_today="$(grep ${pak}/package-xhash.xha ${HASHTREE}/full-${date_today}.log| cut -d' ' -f1)"
+						if ! [ "${pak_hash_last}" = "${pak_hash_today}" ]; then
+							echo "${pak/${WORKDIR}}" >> ${HASHTREE}/results/results-${date_today}.log
+						fi
+					done
+				fi
 			done
-			# generate hash for the full tree
-			find ${WORKDIR} -mindepth 2 -maxdepth 2 -type f -name *.xha -exec xxh64sum {} \; \
-				| tee -a ${WORKDIR}/tree-xhash.xha ${HASHTREE}/full-${date_today}.log >/dev/null
-			# generate results file, based on the diffs from the full-last.log
-			# if this file doesn't exist nothing happen.
-			if [ -e ${HASHTREE}/full-last.log ]; then
-				mkdir -p ${HASHTREE}/results
-				touch ${HASHTREE}/results/results-${date_today}.log
-				# list every category, save hashes of today and yesterday, compare and
-				# if it doesn't match, check every package in that category
-				for cat in $(find ${WORKDIR} -mindepth 1 -maxdepth 1 -type d); do
-					cat_hash_last="$(grep ${cat}/category-xhash.xha ${HASHTREE}/full-last.log | cut -d ' ' -f1)"
-					cat_hash_today="$(grep ${cat}/category-xhash.xha ${HASHTREE}/full-${date_today}.log| cut -d' ' -f1)"
-					if ! [ "${cat_hash_last}" = "${cat_hash_today}" ]; then
-						for pak in $(find ${cat} -mindepth 1 -maxdepth 1 -type d); do
-							pak_hash_last="$(grep ${pak}/package-xhash.xha ${HASHTREE}/full-last.log | cut -d ' ' -f1)"
-							pak_hash_today="$(grep ${pak}/package-xhash.xha ${HASHTREE}/full-${date_today}.log| cut -d' ' -f1)"
-							if ! [ "${pak_hash_last}" = "${pak_hash_today}" ]; then
-								echo "${pak/${WORKDIR}}" >> ${HASHTREE}/results/results-${date_today}.log
-							fi
-						done
-					fi
-				done
-			fi
-			rm -rf ${WORKDIR}
 		fi
+		rm -rf ${WORKDIR}
 	fi
 }
 
