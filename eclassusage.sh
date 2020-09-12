d3dea195017f24d741d9fce17131963c0c354bad
Michael Mair-Keimberger <m.mairkeimberger@gmail.com>
2020-09-12 09:31:06 +0200

    eclassusage: update eclasses

--- eclassusage.sh
+++ eclassusage.sh
@@ -71,8 +71,9 @@ array_names
 
 array_eclasses(){
 	ECLASSES=( \
+		"optfeature;optfeature"
 		"ltprune;prune_libtool_files" \
-		"eutils;emktemp:edos2unix:strip-linguas:make_wrapper:path_exists:use_if_iuse:optfeature:ebeep:in_iuse" \
+		"eutils;emktemp:edos2unix:strip-linguas:make_wrapper:path_exists:use_if_iuse:ebeep:in_iuse" \
 		"estack;estack_push:estack_pop:evar_push:evar_push_set:evar_pop:eshopts_push:eshopts_pop:eumask_push:eumask_pop:isdigit" \
 		"preserve-libs;preserve_old_lib:preserve_old_lib_notify" \
 		"vcs-clean;ecvs_clean:esvn_clean:egit_clean" \
