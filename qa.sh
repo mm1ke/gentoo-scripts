bc00aff1631e7ef0fb4753bcc2b31e15a10ffe1e
Michael Mair-Keimberger <mmk@levelnine.at>
2021-07-04 23:11:38 +0200

    remove rust from checks

--- qa.sh
+++ qa.sh
@@ -9,7 +9,6 @@ REPOSITORIES=(
 	"kde|https://github.com/gentoo/kde"
 	"guru|https://github.com/gentoo/guru"
 	"science|https://github.com/gentoo/sci"
-	"rust|https://github.com/gentoo/gentoo-rust"
 	"pentoo|https://github.com/pentoo/pentoo-overlay"
 	)
 
